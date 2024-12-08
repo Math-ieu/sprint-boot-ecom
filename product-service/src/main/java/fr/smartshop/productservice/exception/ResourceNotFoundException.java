7cf72bf77a2cc7c114615013393e31f6e8d2fe12
mathieu <atsoumathieu228@gmail.com>
2024-12-08 18:03:23 +0100

    update

--- product-service/src/main/java/fr/smartshop/productservice/exception/ResourceNotFoundException.java
+++ product-service/src/main/java/fr/smartshop/productservice/exception/ResourceNotFoundException.java
@@ -1,5 +1,26 @@
 package fr.smartshop.productservice.exception;
 
-public class ResourceNotFoundException {
-    
+/**
+ * Exception personnalisée pour gérer les cas où une ressource n'est pas trouvée.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    /**
+     * Constructeur avec un message personnalisé.
+     *
+     * @param message le message d'erreur
+     */
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructeur avec un message personnalisé et une cause.
+     *
+     * @param message le message d'erreur
+     * @param cause   la cause de l'erreur
+     */
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
