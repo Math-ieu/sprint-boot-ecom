82a250c2c7b7ac376230e1be0aa61c3b909ba1eb
mathieu <atsoumathieu228@gmail.com>
2024-12-08 17:08:25 +0100

    update

--- product-service/src/main/java/fr/smartshop/productservice/dto/CategoryDTO.java
+++ product-service/src/main/java/fr/smartshop/productservice/dto/CategoryDTO.java
@@ -1,5 +1,16 @@
 package fr.smartshop.productservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
-    
+    private Long id;
+    private String name;
+    private List<String> productNames; // List of product names in this category
 }
