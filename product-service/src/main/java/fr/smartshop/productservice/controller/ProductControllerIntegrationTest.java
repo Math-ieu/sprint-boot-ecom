86fd8a5672ae50427054c7e10184aa3a7062c3c4
Joach27 <joachinagbevaide@gmail.com>
2024-12-09 17:12:19 +0100

    Test

--- product-service/src/main/java/fr/smartshop/productservice/controller/ProductControllerIntegrationTest.java
+++ product-service/src/main/java/fr/smartshop/productservice/controller/ProductControllerIntegrationTest.java
@@ -9,13 +9,15 @@
 
 // import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
 // import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 // import org.springframework.boot.test.context.SpringBootTest;
+
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 // import org.springframework.http.MediaType;
-// import org.springframework.test.web.servlet.MockMvc;
 
 // import fr.smartshop.productservice.dto.ProductDTO;
 // import fr.smartshop.productservice.model.Category;
